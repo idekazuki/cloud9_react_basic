168ed46b863a38f6f633307d0ab6c53921f1852d
idekazuki <kuzikaide@gmail.com>
2020-07-03 15:33:58 +0900

    function

--- react-basic/src/components/Basic1.js
+++ react-basic/src/components/Basic1.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Basic1 = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+          <h1>Hello React1</h1>
+          <h1>Hello React2</h1>  
+        </>
     )
 }
 
